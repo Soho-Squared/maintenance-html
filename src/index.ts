e7f236d5babcb00911c68d0ba1d558a66b8bc0f2
William Clem <49172803+clemwj@users.noreply.github.com>
2025-07-31 16:11:17 -0700

    chore: update message

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export default {
 		const html = `<!DOCTYPE html>
 		<body>
 		  <h1>We're Sorry</h1>
-		  <p>The system is down until data flow resumes.</p>
+		  <p>The system is down for maintenance.</p>
 		</body>`;
 
 		return new Response(html, {
